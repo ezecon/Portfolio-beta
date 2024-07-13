776c28ddd884aeb0cbe4046b0b6d19ae44d6a4c5
ezecon <nationhexus@gmail.com>
2024-07-13 11:55:05 +0600

    first commit

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,10 +1,7 @@
-import { useEffect } from "react";
-import { motion } from "framer-motion"
-import { FaGithubSquare} from "react-icons/fa";
-import { Button } from "@material-tailwind/react";
-import { Link } from "react-router-dom";
-
-
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { CgDetailsMore } from "react-icons/cg";
+import { Button, Dialog, DialogBody, DialogHeader } from "@material-tailwind/react";
 
 export default function Project() {
   useEffect(() => {
@@ -17,183 +14,105 @@ export default function Project() {
       textContainer.innerHTML = newText;
     }
   }, []);
+  
+  const [open, setOpen] = useState(false);
+ 
+  const handleOpen = () => setOpen(!open);
+  
+  const handleClose = () => setOpen(false);
 
   return (
-    <div className="px-4 md:px-8 lg:px-16">
-      <div className="text-center text-3xl mt-20 text-[goldenrod]">
-        <h1 id="rotating-text1" className="text-2xl md:text-3xl lg:text-4xl">
-          PROJECTS
-        </h1>
-        <p className="text-sm md:text-base">My Works</p>
-      </div>
-      <div className="flex items-center justify-center mt-4 space-y-4 md:space-y-0 md:space-x-4">
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-      
-                <motion.button
-                        whileHover={{ scale: 1.9 }}
-                        whileTap={{ scale: 0.9 }}
-                    >
-                    <div className="card bg-base-100 w-96 shadow-xl bg-slate-100  shadow-[goldenrod] rounded">
-                            <figure className="p-4 rounded">
-                                <img
-                                src="https://i.ibb.co/16PXC3t/Whats-App-Image-2024-07-13-at-10-22-24.jpg"
-                                alt="Project 1" />
-                            </figure>
-                            <div className="card-body">
-                            <h2 className="text-[goldenrod] playwrite-cl-title">
-                                Halal Brothers
-                                </h2>
-                                <p>Total Restaurant Management System</p>
-                                <div className="flex justify-end pr-4 pb-4 gap-3">
-                                    <div className="text-3xl text-[goldenrod]">
-                                        <FaGithubSquare/>
-                                    </div>
-                                    <div className="text-xl text-[goldenrod]">
-                                        <Button>Live</Button>
-                                    </div>
-                                </div>
-                            </div>
-                            </div>
-                        </motion.button>
-                <motion.button
-                        whileHover={{ scale: 1.9 }}
-                        whileTap={{ scale: 0.9 }}
-                    >
-                    <div className="card bg-base-100 w-96 shadow-xl bg-slate-100  shadow-[goldenrod] rounded">
-                            <figure className="p-4 rounded">
-                                <img
-                                src="https://i.ibb.co/jZkg3dt/Whats-App-Image-2024-07-13-at-10-42-36.jpg"
-                                alt="Project 1" />
-                            </figure>
-                            <div className="card-body">
-                            <h2 className="text-[goldenrod] playwrite-cl-title">
-                                CY-RENT
-                                </h2>
-                                <p>A Unique Project</p>
-                                <div className="flex justify-end pr-4 pb-4 gap-3">
-                                    <div className="text-3xl text-[goldenrod]">
-                                        <FaGithubSquare/>
-                                    </div>
-                                    <div className="text-xl text-[goldenrod]">
-                                        <Button>Live</Button>
-                                    </div>
-                                </div>
-                            </div>
-                            </div>
-                        </motion.button>
-                <motion.button
-                        whileHover={{ scale: 1.9 }}
-                        whileTap={{ scale: 0.9 }}
-                    >
-                    <div className="card bg-base-100 w-96 shadow-xl bg-slate-100  shadow-[goldenrod] rounded">
-                            <figure className="p-4 rounded">
-                                <img
-                                src="https://i.ibb.co/262RvHV/Whats-App-Image-2024-07-13-at-10-46-02.jpg"
-                                alt="Project 1" />
-                            </figure>
-                            <div className="card-body">
-                                <h2 className="text-[goldenrod] playwrite-cl-title">
-                                Small E-Commerce
-                                </h2>
-                                <p>It is a small e-commerce website</p>
-                                <div className="flex justify-end pr-4 pb-4 gap-3">
-                                    <div className="text-3xl text-[goldenrod]">
-                                        <FaGithubSquare/>
-                                    </div>
-                                    <div className="text-xl text-[goldenrod]">
-                                        <Button>Live</Button>
-                                    </div>
-                                </div>
-                            </div>
-                            </div>
-                        </motion.button>
-                <motion.button
-                        whileHover={{ scale: 1.9 }}
-                        whileTap={{ scale: 0.9 }}
-                    >
-                    <div className="card bg-base-100 w-96 shadow-xl bg-slate-100  shadow-[goldenrod] rounded">
-                            <figure className="px-4 py-6 rounded">
-                                <img
-                                src="https://i.ibb.co/HpTfgL0/Whats-App-Image-2024-07-13-at-11-05-49.jpg"
-                                alt="Project 1" />
-                            </figure>
-                            <div className="card-body">
-                                <h2 className="text-[goldenrod] playwrite-cl-title">
-                                2.74
-                                </h2>
-                                <p>Another project of a restaurant</p>
-                                <div className="flex justify-end pr-4 pb-4 gap-3">
-                                    <div className="text-3xl text-[goldenrod]">
-                                        <FaGithubSquare/>
-                                    </div>
-                                    <div className="text-xl text-[goldenrod]">
-                                        <Button>Live</Button>
-                                    </div>
-                                </div>
-                            </div>
-                            </div>
-                        </motion.button>
-                        <motion.button
-                                whileHover={{ scale: 1.9 }}
-                                whileTap={{ scale: 0.9 }}
-                            >
-                                <div className="card bg-base-100 w-96 shadow-xl bg-slate-100 shadow-[goldenrod] rounded">
-                                    <figure className="px-4 py-20 rounded">
-                                        <img
-                                            src="https://i.ibb.co/tKKqWXz/Owner-dash-Board.png"
-                                            alt="Project 1"
-                                        />
-                                    </figure>
-                                    <div className="card-body">
-                                        <h2 className="text-[goldenrod] playwrite-cl-title">
-                                            BARI-WALA
-                                        </h2>
-                                        <p>A Unique Project</p>
-                                        <div className="flex justify-end pr-4 pb-4 gap-3">
-                                            <div className="text-3xl text-[goldenrod]">
-                                                <a href="https://github.com/ezecon/Bari-Wala-a-Home-Security" target="_blank" rel="noopener noreferrer">
-                                                    <FaGithubSquare />
-                                                </a>
-                                            </div>
-                                            <div className="text-xl text-[goldenrod]">
-                                                <a href="https://bari-wala-a-home-security.vercel.app/" target="_blank" rel="noopener noreferrer">
-                                                    <Button>Live</Button>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </motion.button>
-
-                <motion.button
-                        whileHover={{ scale: 1.9 }}
-                        whileTap={{ scale: 0.9 }}
-                    >
-                    <div className="card bg-base-100 w-96 shadow-xl bg-slate-100  shadow-[goldenrod] rounded">
-                            <figure className="p-4 rounded">
-                                <img
-                                src="https://i.ibb.co/z82pTJq/Whats-App-Image-2024-07-13-at-10-54-17.jpg"
-                                alt="Project 1" />
-                            </figure>
-                            <div className="card-body">
-                                <h2 className="text-[goldenrod] playwrite-cl-title">
-                                Weather ForeCasting
-                                </h2>
-                                <p>It is a small e-commerce website</p>
-                                <div className="flex justify-end pr-4 pb-4 gap-3">
-                                    <div className="text-3xl text-[goldenrod]">
-                                        <FaGithubSquare/>
-                                    </div>
-                                    <div className="text-xl text-[goldenrod]">
-                                        <Button>Live</Button>
-                                    </div>
-                                </div>
-                            </div>
-                            </div>
-                        </motion.button>
-
-           </div>
-        
+    <div>
+      <div className="px-4 md:px-8 lg:px-16">
+        <div className="text-center text-3xl mt-20 text-[goldenrod]">
+          <h1 id="rotating-text1" className="text-2xl md:text-3xl lg:text-4xl">
+            PROJECTS
+          </h1>
+          <p className="text-sm md:text-base">My Works</p>
+        </div>
+        <div className="flex items-center justify-center mt-4 space-y-4 md:space-y-0 md:space-x-4">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+            {[
+              {
+                img: "https://i.ibb.co/16PXC3t/Whats-App-Image-2024-07-13-at-10-22-24.jpg",
+                title: "Halal Brothers",
+                description: "Total Restaurant Management System",
+              },
+              {
+                img: "https://i.ibb.co/jZkg3dt/Whats-App-Image-2024-07-13-at-10-42-36.jpg",
+                title: "CY-RENT",
+                description: "A Unique Project",
+              },
+              {
+                img: "https://i.ibb.co/262RvHV/Whats-App-Image-2024-07-13-at-10-46-02.jpg",
+                title: "Small E-Commerce",
+                description: "It is a small e-commerce website",
+              },
+              {
+                img: "https://i.ibb.co/HpTfgL0/Whats-App-Image-2024-07-13-at-11-05-49.jpg",
+                title: "2.74",
+                description: "Another project of a restaurant",
+              },
+              {
+                img: "https://i.ibb.co/tKKqWXz/Owner-dash-Board.png",
+                title: "BARI-WALA",
+                description: "A Unique Project",
+                link: "https://github.com/ezecon/Bari-Wala-a-Home-Security",
+                demo: "https://bari-wala-a-home-security.vercel.app/",
+              },
+              {
+                img: "https://i.ibb.co/z82pTJq/Whats-App-Image-2024-07-13-at-10-54-17.jpg",
+                title: "Weather ForeCasting",
+                description: "It is a small e-commerce website",
+              },
+            ].map((project, index) => (
+              <motion.button
+                whileHover={{ scale: 1.9 }}
+                whileTap={{ scale: 0.9 }}
+                key={index}
+                onClick={handleOpen}
+              >
+                <div className="card bg-base-100 w-96 shadow-xl bg-slate-100 shadow-[goldenrod] rounded">
+                  <figure className="p-4 rounded">
+                    <img src={project.img} alt={project.title} />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="text-[goldenrod] playwrite-cl-title">
+                      {project.title}
+                    </h2>
+                    <p>{project.description}</p>
+                    <div className="flex justify-end pr-4 pb-4 gap-3">
+                      <div className="text-3xl text-[goldenrod]">
+                        <CgDetailsMore />
+                      </div>
+                      <div className="text-xl text-[goldenrod]">
+                        {project.link ? (
+                          <a href={project.link} target="_blank" rel="noopener noreferrer">
+                            <Button>Details</Button>
+                          </a>
+                        ) : (
+                          <Button>Details</Button>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </motion.button>
+            ))}
+          </div>
+          <Dialog
+            open={open}
+            handler={handleOpen}
+            className="fixed inset-0 flex items-center justify-center bg-transparent"
+          >
+            <div className="bg-white rounded-lg p-4 w-full max-w-md mx-auto">
+              <DialogHeader>Details</DialogHeader>
+              <DialogBody>
+                <Button onClick={handleClose}>Exit</Button>
+              </DialogBody>
+            </div>
+          </Dialog>
+        </div>
       </div>
     </div>
   );
