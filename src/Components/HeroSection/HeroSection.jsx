1edd5c91df29ab84963eef8809c755724ddab0e8
Md Econozzaman Econ <92664558+ezecon@users.noreply.github.com>
2024-07-28 00:00:19 +0600

    Update HeroSection.jsx

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -33,7 +33,7 @@ export default function HeroSection() {
       >
         <img
           className="w-3/4 md:w-1/2 rounded-md py-4"
-          src="https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/438301459_2478369702550969_5250834645199073879_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeExya5algHCh56bpZB9wFtM8Oh9iN5liZfw6H2I3mWJl5CJ6KGAbIf9d7U6IktTMAthdXEBi6biR2uBDbWuYldB&_nc_ohc=MJhiX4lvpXgQ7kNvgF6Ve59&_nc_ht=scontent.fdac8-1.fna&oh=00_AYDHB_Q4OVX8W-DO-UTWm3wLVjvov6Tj2AOuKl2MgZ1eUA&oe=66955D01"
+          src="https://i.ibb.co/Kbxz0NB/438301459-2478369702550969-5250834645199073879-n.jpg"
           alt="Profile Image"
         />
       </motion.div>
